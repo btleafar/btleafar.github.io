25e2d5571dc9b983c69f8709ba52ed6a2a7808d7
Rafael Torres Balabukha <rafael.tb@outlook.com>
2023-09-12 12:14:58 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create the main directory
+mkdir my-website
+
+# Navigate into the main directory
+cd my-website
+
+# Create the index.html file
+touch index.html
+
+# Create the 'css' and 'js' directories
+mkdir css js
+
+# Create the styles.css and scripts.js files
+touch css/styles.css js/scripts.js
+
+# Print a message to indicate the script has finished running
+echo "Directory structure set up successfully!"
